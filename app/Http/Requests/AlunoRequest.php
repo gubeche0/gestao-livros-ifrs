6e86d3478c712b49056d059406c6640c8c4bb9a5
Laura <lauracpiffer@gmail.com>
2019-07-07 00:40:35 -0300

    arrumei a validação do livro

--- app/Http/Requests/AlunoRequest.php
+++ app/Http/Requests/AlunoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LivroRequest extends FormRequest
+class AlunoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class LivroRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,11 +24,7 @@ class LivroRequest extends FormRequest
     public function rules()
     {
         return [
-            'isbn' => ['required', 'numeric'],
-            'nome' => ['required'],
-            'volume' => ['required'],
-            'autor' => ['required'],
-            'foto' => ['file'],
+            //
         ];
     }
 }

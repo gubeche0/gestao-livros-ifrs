f4a5d2a94e77af21f6e37cad4599e2fa46688b27
Gustavo Beche Lopes <gubeche0@gmail.com>
2019-08-13 15:37:58 -0300

    ajuste no botao de registrar exemplares na listagem dos livros

--- resources/views/livro/index.blade.php
+++ resources/views/livro/index.blade.php
@@ -43,9 +43,9 @@
                         <td>{{ $livro->estoque() }}</td>
                         <td>{{ $livro->disponiveis()}}</td>
                         <td>
-                            <button class="text-dark" data_livro="{{$livro->id}}" id="btn-registrar" class="btn btn-primary col">
-                                <i class="fas fa-plus" aria-hidden="true"></i> Registrar exemplar</button> |
-
+                            <a class="text-dark" data_livro="{{$livro->id}}" id="btn-registrar" href="#">
+                                <i class="fas fa-plus" aria-hidden="true"></i> Registrar exemplar
+                            </a> |
                             <a class="text-dark" href='{{ route('livro.edit', ['livro' => $livro->id]) }}'><i class="fas fa-edit"
                                     aria-hidden="true"></i> Editar</a> |
                             <a class="text-dark" href="#" onclick="excluir('{{ route('livro.delete', ['livro' => $livro->id]) }}')"><i class="fas fa-trash"

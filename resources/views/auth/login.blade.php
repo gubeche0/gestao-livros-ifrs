96541c39ebc4c6d49718ad5cce4b8da451c3b04c
Laura <lauracpiffer@gmail.com>
2019-11-24 12:41:59 -0300

    layout do Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Gestão Livros IFRS</title>
-    <link rel="shortcut icon" href="/res/img/ifrs.ico?v=2" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('img/ifrs.ico')}}" type="image/x-icon">
     <meta name="viewport" content="width=device-width">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
@@ -15,22 +15,42 @@
 
 <body>
     
-    @include('layouts.statusMessages')
     <form class="form-signin col-sm-3 mx-auto " style="margin-top: 8%;" method="post" action="/login">
         @csrf
         <div class="text-center">
-            <img class="mb-4" src="{{ asset('img/if-logo.png') }}" alt="Logo do IFRS" width="100" />
-
-            <h1 class="h3 mb-3 font-weight-normal">Login</h1>
-            <label for="email" class="sr-only">Endereço de email</label>
-            <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Endereço de email" required autofocus>
-            <label for="pass" class="sr-only">Senha</label>
-            <input type="password" id="password" name="password" class="form-control @error('email') is-invalid @enderror" placeholder="Senha" required>
+                <img class="mb-4" src="{{ asset('img/if-logo.png') }}" alt="Logo do IFRS" width="100" />
+                <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-3">
+                <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-at"></i></span>
+                    </div>
+                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="text" name="email" value="{{ old('email') }}" value="admin@argon.com" autofocus>
+                </div>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-unlock-alt"></i></span>
+                    </div>
+                    <input id="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Senha') }}" value="{{ old('password') }}" type="password">
+                </div>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
             
+            <div class="text-center">
+                <button type="submit" class="btn btn-success btn-lg">{{ __('Entrar') }}</button>
+            </div>
         </div>
-        
-        <button class="btn btn-lg btn btn-success btn-block mt-3" type="submit">Entrar</button>
-        
     </form>
 
 
@@ -43,4 +63,3 @@
 </body>
 
 </html>
-
